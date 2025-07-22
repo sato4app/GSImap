bcbb8a397c90d3a28c8e9da65582632c5dd9a366
sato4app <sato.apps@quad.razor.jp>
2025-07-22 22:37:35 +0900

    remove definitions for pwa

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 // 1. Service Workerの登録
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw.js').then(registration => {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }).catch(err => {
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
+// PWA対応不要のため削除
 
 // 2. 地図の初期化
 // 箕面大滝の座標 (緯度, 経度)
