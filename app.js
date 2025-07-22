6f17db52a1d0c7cd71334a49cc2a01bca275ed7f
sato4app <sato.apps@quad.razor.jp>
2025-07-22 22:21:14 +0900

    define folder of sw.js in app.js, and manifest.json in index.html

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // 1. Service Workerの登録
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(registration => {
+        navigator.serviceWorker.register('sw.js').then(registration => {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }).catch(err => {
             console.log('ServiceWorker registration failed: ', err);
